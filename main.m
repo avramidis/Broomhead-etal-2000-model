63972534c78f88a66e4869326b85658726bc43a2
Eleftherios Avramidis <el.avramidis@gmail.com>
2015-11-19 12:12:11 +0000

    Update main.m
    Added comments

--- main.m
+++ main.m
@@ -1,12 +1,12 @@
 close all
 clear all
 
-t=[0 6];
-initial=[0 0 0 0 0 2];
-p=[120 1.5 0.0045 0.05 600 9];
+t=[0 6];  // Integration time span. 0 to 6 seconds.
+initial=[0 0 0 0 0 2]; // Broomhead et al. model's initial values. Set for 2 degrees eye movement.
+p=[120 1.5 0.0045 0.05 600 9];  // Broomhead et al. model's parameter values. Jerk waveform.
 
 [T,Y]  = broomhead( t, initial, p );
 
-plot(T,Y(:,1))
+plot(T,Y(:,1)) // Plot the simulated gaze angle
 
 
