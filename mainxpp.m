eaa7812454bbdc1ff3f01aa1db9ee8faffb5960d
Eleftherios Avramidis <el.avramidis@gmail.com>
2015-11-19 22:06:00 +0000

    Update mainxpp.m
    Comments correction

--- mainxpp.m
+++ mainxpp.m
@@ -1,10 +1,10 @@
 close all
 clear all
 
-t=6;                            // Integration time span. 0 to 6 seconds.
-initial=[0 0 0 0 0 2];          // Broomhead et al. model's initial values. Set for 2 degrees eye movement.
-p=[120 1.5 0.0045 0.05 600 9];  // Broomhead et al. model's parameter values. Jerk waveform.
+t=6;                            % Integration time span. 0 to 6 seconds.
+initial=[0 0 0 0 0 2];          % Broomhead et al. model's initial values. Set for 2 degrees eye movement.
+p=[120 1.5 0.0045 0.05 600 9];  % Broomhead et al. model's parameter values. Jerk waveform.
 
-[T,Y] = broomheadxpp( t,initial, p ); // Call the XPPAUT solver
+[T,Y] = broomheadxpp( t,initial, p ); % Call the XPPAUT solver
 
-plot(T,Y(:,1))  // Plot the simulated gaze angle
+plot(T,Y(:,1))  % Plot the simulated gaze angle
