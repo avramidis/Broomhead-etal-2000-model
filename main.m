1c405ca43f7d95fd29a91a3c3dd64ccaa8377117
Eleftherios Avramidis <el.avramidis@gmail.com>
2017-12-22 10:00:05 +0000

    At the start clears variables and not all.
    This is better than clearing all objects in the MATLAB runtime.

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 close all
-clear all
+clear variables
 
 t=[0 6];                        % Integration time span. 0 to 6 seconds.
 initial=[0 0 0 0 0 2];          % Broomhead et al. model's initial values. Set for 2 degrees eye movement.
@@ -9,5 +9,3 @@ p=[120 1.5 0.0045 0.05 600 9];  % Broomhead et al. model's parameter values. Jer
 [T,Y]  = broomhead( t, initial, p );  % Call the MATLAB solver
 
 plot(T,Y(:,1)) % Plot the simulated gaze angle
-
-
